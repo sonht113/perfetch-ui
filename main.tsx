f5757ebd8f84fd773494c8658a98a36e06d25064
Ho Trong Son <sonht1@runsystem.net>
2025-01-10 16:41:40 +0700

    feature: init commit

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./lib/index.css";
+import "./packages/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
