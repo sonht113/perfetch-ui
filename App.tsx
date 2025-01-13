afb05481a6a28403183441f1dbd1ea5c26c97cab
nuicoder <trongson18101999@gmail.com>
2025-01-13 11:49:54 +0700

    feat: init commit

---
+++ App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p className="text-red-500">hello</p>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./packages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
